fde72e587f53b5e3d5905661aef9e23c10b0643d
NeilHartley <55210218+NeilwHartley@users.noreply.github.com>
2019-09-22 17:54:28 +0100

    v.2

--- clockscript.js
+++ clockscript.js
@@ -1,15 +1,38 @@
-
 var t = new Date();
 var h = t.getHours().toString();
 var m = t.getMinutes().toString();
 var s = t.getSeconds().toString();
 
-
 function timeNow() {
-	setInterval(timeNow, 1000);
+	setTimeout(timeNow, 1000);
 	document.getElementById("time").innerHTML = h + " : " + m + " : " + s;
 	t = new Date ();
 	h = t.getHours().toString();
 	m = t.getMinutes().toString();
 	s = t.getSeconds().toString();
+	startHour();
+	startMinute();
+	startSecond();
+	}
+
+function startHour() {
+		h12hr = 22 - h;
+		hDeg = ((360/12) * h12hr + 180);
+		hDegInsert = "rotate(" + hDeg + "deg)";
+		document.getElementById("hourHand").style.transform = hDegInsert;
+	}
+
+function startMinute() {
+	mDeg = ((360/60) * m) + 180;
+	mDegInsert = "rotate(" + mDeg + "deg)";
+	document.getElementById("minuteHand").style.transform = mDegInsert;
+}
+
+function startSecond() {
+	sDeg = ((360/60) * s) + 180;
+	sDegInsert = "rotate(" + sDeg + "deg)";
+	document.getElementById("secondHand").style.transform = sDegInsert;
+	console.log(s);
+	console.log(sDeg);
+	console.log(sDegInsert);
 }
