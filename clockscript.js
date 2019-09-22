62550ae51d03733d420f6ab34f970cc103ddf7c2
NeilHartley <55210218+NeilwHartley@users.noreply.github.com>
2019-09-22 21:09:41 +0100

    v2.2

--- clockscript.js
+++ clockscript.js
@@ -13,14 +13,18 @@ function timeNow() {
 	startHour();
 	startMinute();
 	startSecond();
-	}
+}
 
 function startHour() {
-		h12hr = 22 - h; //This should be (24 - h)
-		hDeg = ((360/12) * h12hr + 180);
-		hDegInsert = "rotate(" + hDeg + "deg)";
-		document.getElementById("hourHand").style.transform = hDegInsert;
-	}
+    if (h > 12) {
+    	h12hr = h - 12;
+    } else {
+    	h12hr = h;
+    }
+	hDeg = ((360/12) * h12hr + 180);
+	hDegInsert = "rotate(" + hDeg + "deg)";
+	document.getElementById("hourHand").style.transform = hDegInsert;
+}
 
 function startMinute() {
 	mDeg = ((360/60) * m) + 180;
@@ -38,15 +42,10 @@ function startSecond() {
 }
 
 /*
-	
-	The button is pretty redundant now that its all working, you could call the start function immediately and remove the button.
-
-	Some funky indentation going on in startHour
-
 	Semi colons for the function definitions.
 
 	Challenge
 	The hour hand doesnt appear as it would usually on a clock; ie between the hours with respect to minuites.
 	See if you can incorperate the minuite progression into the angle :) some fun maths for you there :)
 	If you do undertake this challenge, I wouldnt worry about the seconds, though you could technically incorperate it, im sure its negligable.
-*/
+JB */
