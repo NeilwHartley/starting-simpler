60d92540c98896dcfe6746bdbd99df15439e9cbc
NeilHartley <55210218+NeilwHartley@users.noreply.github.com>
2019-09-22 22:38:30 +0100

    v2.3

--- clockscript.js
+++ clockscript.js
@@ -2,6 +2,7 @@ var t = new Date();
 var h = t.getHours().toString();
 var m = t.getMinutes().toString();
 var s = t.getSeconds().toString();
+var i = s * 6;
 
 function timeNow() {
 	setTimeout(timeNow, 1000);
@@ -13,6 +14,7 @@ function timeNow() {
 	startHour();
 	startMinute();
 	startSecond();
+	secs();
 }
 
 function startHour() {
@@ -21,24 +23,34 @@ function startHour() {
     } else {
     	h12hr = h;
     }
-	hDeg = ((360/12) * h12hr + 180);
+    minsDeg = 0.5 * m; //Each hour is 360 degrees so each minute is equal to 0.5 of a degree NH
+	hDeg = (360 / 12) * h12hr + 180 + minsDeg;
 	hDegInsert = "rotate(" + hDeg + "deg)";
 	document.getElementById("hourHand").style.transform = hDegInsert;
 }
 
 function startMinute() {
-	mDeg = ((360/60) * m) + 180;
+	secsDeg = 0.1 * s;
+	mDeg = (360 / 60) * m + 180 + secsDeg;
 	mDegInsert = "rotate(" + mDeg + "deg)";
 	document.getElementById("minuteHand").style.transform = mDegInsert;
 }
 
+// I started trying things hence new variables and secs function. Ended up being more confused 
+// Used CSS to do this unlike with minute and hour hands NH
 function startSecond() {
-	sDeg = ((360/60) * s) + 180;
+	sDeg2 = i + 180;
+	sDeg = ((360 / 60) * s) + 180;
 	sDegInsert = "rotate(" + sDeg + "deg)";
 	document.getElementById("secondHand").style.transform = sDegInsert;
 	console.log(s);
 	console.log(sDeg);
 	console.log(sDegInsert);
+}
+
+function secs() {
+	setTimeout(secs, 1000);
+	i = i + 6; 
 }
 
 /*
